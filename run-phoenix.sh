f3fa35abfd9501203f33308975580d73a0c4e839
Florian Klemme <mail@florianklemme.de>
2015-06-30 01:43:26 +0200

    Added some logging. Changed Phoenix password to 'password'.

--- run-phoenix.sh
+++ run-phoenix.sh
@@ -7,11 +7,11 @@ if [ ! -d "pyramid-phoenix" ]; then
     git clone https://github.com/bird-house/pyramid-phoenix.git
 
     ### Install
-    make -C malleefowl
+    make -C malleefowl 2>&1 | tee make_mallefowl.log
     make -C pyramid-phoenix custom.cfg
-    # Set password to "phoenix"
-    sed -e "s/^\(phoenix-password = \).*/\1sha256:45bcc8442434:dc787c42f0237dfd2b2639685646f0af893ae9b58d5d1c3878d7726f261239ec/" -i pyramid-phoenix/custom.cfg
-    make -C pyramid-phoenix
+    # Set password to "password"
+    sed -e "s/^\(phoenix-password = \).*/\1sha256:5a0e483e03a4:1929ff5ef14c6ceb70eb70c0131fc77ded67c3dfedad53ad49b490a7d8d9996f/" -i pyramid-phoenix/custom.cfg
+    make -C pyramid-phoenix 2>&1 | tee make_phoenix.log
 fi
 
 ### Start
